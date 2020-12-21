0505ea9382986f003781b35428304fb76655e402
Esterci <thiago.esterci@engenharia.ufjf.br>
2020-12-21 14:20:27 -0300

    Update Anomaly_Detection.py

--- Anomaly_Detection.py
+++ Anomaly_Detection.py
@@ -39,10 +39,10 @@ def main():
     # Range of SODA granularities
     min_granularity = 1
 
-    max_granularity = 2
+    max_granularity = 1
 
     # Number of iteration
-    iterations = 2
+    iterations = 1
 
     # Number of process to create in the multiprocessing step
     PROCESSES = 1
@@ -110,8 +110,10 @@ def main():
         # Plots PCA results
         dm.PCA_Analysis(xyz_mantained_variation,xyz_attributes_influence)
 
+        for gra in range(min_granularity, max_granularity + 1):
+            dm.SODA_Granularity_Iteration(proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i)
 
-        print('Creating pool with %d processes\n' % PROCESSES)
+        """print('Creating pool with %d processes\n' % PROCESSES)
 
         with multiprocessing.Pool(PROCESSES) as pool:
 
@@ -120,7 +122,7 @@ def main():
 
             TASKS = [(dm.SODA_Granularity_Iteration, (proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i,0)) for gra in range(min_granularity, max_granularity + 1)]
 
-            pool.map(calculatestar, TASKS)
+            pool.map(calculatestar, TASKS)"""
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
