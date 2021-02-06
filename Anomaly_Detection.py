4ac6083125bf880501210a135e46175c6ceb4a7c
matheusmaltaf <67390856+matheusmaltaf@users.noreply.github.com>
2021-02-06 13:02:12 -0300

    06_02

--- Anomaly_Detection.py
+++ Anomaly_Detection.py
@@ -42,8 +42,9 @@ def main():
 
     max_granularity = 30
 
+    gra_list = [1, 23] #[i for i in range(min_granularity, max_granularity + 1)]
     # Number of iteration
-    iterations = 5
+    iterations = 10
 
     # Number of process to create in the multiprocessing step
     PROCESSES = 1
@@ -57,14 +58,14 @@ def main():
     # Loading data into the code
 
     ### Background    
-    b_name='Reduced_Input_Background_2.csv'
+    b_name='Input_Background_1.csv'
 
     background = np.genfromtxt(b_name, delimiter=',')
     background = background[1:,:]
     print(background.shape)
 
     ### Signal
-    s_name='Reduced_Input_Signal_2.csv'
+    s_name='Input_Signal_1.csv'
 
     signal = np.genfromtxt(s_name, delimiter=',')
     #signal = signal[1:,:]
@@ -110,8 +111,8 @@ def main():
         # Plots PCA results
         dm.PCA_Analysis(xyz_mantained_variation,xyz_attributes_influence)
 
-        for gra in range(min_granularity, max_granularity+1):
-            dm.SODA_Granularity_Iteration(proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i)
+        #for gra in range(min_granularity, max_granularity+1):
+        #    dm.SODA_Granularity_Iteration(proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i)
         
     
         print('Creating pool with %d processes\n' % PROCESSES)
@@ -122,7 +123,7 @@ def main():
             # Tests
             print("=== Criar Tasks ===")
 
-            TASKS = [(dm.SODA_Granularity_Iteration, (proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i,1)) for gra in range(min_granularity, max_granularity + 1)]
+            TASKS = [(dm.SODA_Granularity_Iteration, (proj_xyz_background_train,proj_xyz_streaming_data, gra,len(background_test),n_i,1)) for gra in gra_list]
 
             pool.map(calculatestar, TASKS)
 
